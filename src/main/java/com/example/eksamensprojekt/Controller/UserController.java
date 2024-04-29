8fd3576cd728eb73a98f8ae0948747c358f38aac
OliverLyksted <olly0001@stud.kea.dk>
2024-04-29 16:30:20 +0200

    added get("projectManager") and projectManager.html

--- src/main/java/com/example/eksamensprojekt/Controller/UserController.java
+++ src/main/java/com/example/eksamensprojekt/Controller/UserController.java
@@ -16,52 +16,67 @@ import java.sql.SQLException;
 public class UserController {
     private UserService userService;
 
-    public UserController(UserService userService){
-        this.userService=userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @GetMapping("/")
-    public String home(){
+    public String home() {
         return "home";
     }
 
     @GetMapping("/login")
-    public String login(){
+    public String login() {
         return "login";
     }
 
     @PostMapping("/login")
-    public String loginPost(@RequestParam String username, @RequestParam String password, HttpSession session, Model model){
+    public String loginPost(@RequestParam String username, @RequestParam String password, HttpSession session, Model model) {
         try {
-            User user= userService.checkLogin(username, password);
-            if (user!=null && user.getRole().equals("admin")){
+            User user = userService.checkLogin(username, password);
+            if (user != null && user.getRole().equals("admin")) {
                 session.setAttribute("user", user);
                 return "redirect:/admin";
-            } else if (user!=null && user.getRole().equals("employee")) {
+            } else if (user != null && user.getRole().equals("employee")) {
                 session.setAttribute("user", user);
                 return "redirect:/employee";
+            } else if (user != null && user.getRole().equals("pjManager")) {
+            session.setAttribute("user", user);
+            return "redirect:/projectManager";
             } else {
                 model.addAttribute("error", "invalid");
                 return "home";
             }
-        } catch (SQLException e){
-            throw  new RuntimeException(e);
+        } catch (
+                SQLException e) {
+            throw new RuntimeException(e);
         }
     }
+
     @GetMapping("/admin")
-    public String admin(HttpSession session, Model model){
+    public String admin(HttpSession session, Model model) {
         User user = (User) session.getAttribute("user");
-        if (user!=null && user.getRole().equals("admin")){
+        if (user != null && user.getRole().equals("admin")) {
             return "admin";
         } else {
             return "redirect:/";
         }
     }
+
     @GetMapping("/employee")
-    public String employee(HttpSession session, Model model){
+    public String employee(HttpSession session, Model model) {
         User user = (User) session.getAttribute("user");
-        if (user!=null && user.getRole().equals("employee")){
+        if (user != null && user.getRole().equals("employee")) {
             return "employee";
+        } else {
+            return "redirect:/";
+        }
+    }
+    @GetMapping("/projectManager")
+    public String projectManager(HttpSession session, Model model) {
+        User user = (User) session.getAttribute("user");
+        if (user != null && user.getRole().equals("pjManager")) {
+            return "projectManager";
         } else {
             return "redirect:/";
         }
