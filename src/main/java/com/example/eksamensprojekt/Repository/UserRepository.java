9ff647d6ff1e879e9e5e5e48ecee45d05ed2e40a
OliverLyksted <olly0001@stud.kea.dk>
2024-04-29 23:14:32 +0200

    created createUser.html and added some style and removed a signup method we don't need anymore

--- src/main/java/com/example/eksamensprojekt/Repository/UserRepository.java
+++ src/main/java/com/example/eksamensprojekt/Repository/UserRepository.java
@@ -49,23 +49,5 @@ public class UserRepository {
 
    }
 
-   /* public User signUp(User user) throws SQLException {
-        try {
-            Connection connection = Connection();
-            String SQL = "INSERT INTO User ( username, password, role_Id) VALUES (?, ?, ?)";
-            PreparedStatement ps = connection.prepareStatement(SQL);
-            ps.setString(1, user.getUsername());
-            ps.setString(2, user.getPassword());
-            ps.setInt(3, user.getRole_ID());
-            ResultSet rs = ps.executeQuery();
 
-            if (rs.next()) {
-                int user_Id = rs.getInt(1);
-                user.setUser_ID(user_Id);
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return user;
-    }*/
 }
--- src/main/java/com/example/eksamensprojekt/Service/UserService.java
+++ src/main/java/com/example/eksamensprojekt/Service/UserService.java
@@ -10,14 +10,6 @@ import java.sql.SQLException;
 public class UserService {
     private UserRepository userRepository;
 
-    public UserService(UserRepository userRepository){
-        this.userRepository=userRepository;
-    }
-    /*public void singUp(User user) throws SQLException {
-        userRepository.signUp(user);
-    }
-
-     */
     public User checkLogin(String username, String password)throws SQLException{
       return userRepository.checkLogin(username, password);
     }
