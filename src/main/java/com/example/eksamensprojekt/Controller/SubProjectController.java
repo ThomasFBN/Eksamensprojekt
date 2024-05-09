a09d259f2b9dd2b8265e9ebffe1df11168c5f267
ThomasFBN <thni0004@stud.kea.dk>
2024-05-09 15:50:58 +0200

    added createSubProject in SubProjectController

--- src/main/java/com/example/eksamensprojekt/Controller/SubProjectController.java
+++ src/main/java/com/example/eksamensprojekt/Controller/SubProjectController.java
@@ -1,13 +1,13 @@
 package com.example.eksamensprojekt.Controller;
 
+import com.example.eksamensprojekt.Model.Project;
 import com.example.eksamensprojekt.Model.SubProject;
 import com.example.eksamensprojekt.Model.Task;
 import com.example.eksamensprojekt.Model.User;
 import com.example.eksamensprojekt.Service.SubProjectService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -19,14 +19,31 @@ public class SubProjectController {
    public SubProjectController(SubProjectService subProjectService){
        this.subProjectService = subProjectService;
    }
-    @GetMapping("/subProjectDetails/{subProjectId}")
-    public String subProjectDetails (@PathVariable int subProjectId, Model model) throws SQLException {
+    @GetMapping("/projectDetails/{projectId}/createSubProject")
+    public String showCreateSubProjectForm(@PathVariable("projectId") int projectId, Model model) {
+        model.addAttribute("projectId", projectId);
+        model.addAttribute("subProject", new SubProject());
+        return "createSubProject";
+    }
+    @PostMapping("/projectDetails/{projectId}/createSubProject")
+    public String createSubProject(@PathVariable("projectId") int projectId, @ModelAttribute SubProject subProject) throws SQLException {
+        subProject.setProjectId(projectId);
+        subProjectService.createSubProject(subProject);
+        int subProjectId = subProject.getSubProjectId();
+        return "redirect:/projects/" + projectId + "/subProjectDetails/" + subProjectId;
+    }
+
+
+
+    @GetMapping("/subProjectDetals/{subProjectId}")
+    public String subProjectDetails(@PathVariable int subProjectId, Model model) throws SQLException {
         List<Task> tasks = subProjectService.findTasksBySubProjectId(subProjectId);
-
+        List<User> users = subProjectService.findUsersBySubProjectId(subProjectId);
         model.addAttribute("tasks", tasks);
-
+        model.addAttribute("users", users);
         return "subProjectDetails";
     }
+
 
 
 
